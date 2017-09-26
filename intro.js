f5c2e339e6f6a2d4cf78d7b38ae8cdc4ba1746f9
Nikolai_Laptop <Nikolai.stecker@gmx.de>
2017-09-26 18:32:05 +0200

    bei button druck wird funktion aus javascript aufgerufen

--- intro.js
+++ intro.js
@@ -13,3 +13,15 @@ var bar = "2";
 
 var result = (foo == bar);
 console.log(result);
+
+function meineErsteFunktion(){
+    console.log("Ich bin eine Funktion");
+}
+
+meineErsteFunktion();
+
+var myButton = document.getElementById("myButton");
+
+myButton.addEventListener('mousedown', function(e){
+    console.log("buttonpressed");
+})
