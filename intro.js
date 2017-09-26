8a440bff1e34549598b37acf4e17dea87d167626
Nikolai_Laptop <Nikolai.stecker@gmx.de>
2017-09-26 18:18:54 +0200

    Video Entfernt

---
+++ intro.js
@@ -0,0 +1,15 @@
+/* Variablen*/
+var a = 1;
+var b = 2;
+
+a = b;
+console.log(a);
+console.log(b);
+
+//Operatoren
+
+var foo = 2;
+var bar = "2";
+
+var result = (foo == bar);
+console.log(result);
