8f2ba99f67e28d03f90e51937ad7b1b1e7ed1fc0
Nikolai_Laptop <Nikolai.stecker@gmx.de>
2017-10-10 17:00:09 +0200

    lautstärke des theremin ist änderbar

--- Audio/theremin.js
+++ Audio/theremin.js
@@ -5,23 +5,17 @@ var gain = context.createGain();
 var width = window.innerWidth
 var height = window.innerHeight
 var maxFrequenz = 2400;
-var maxVolume = 0.01;
 
 oscillator.frequency.value = 880;
-gain.gain.value = maxVolume;
+gain.gain.value = 0.01;
 
 oscillator.connect(gain);
-oscillator.connect(context.destination);
+gain.connect(context.destination);
 oscillator.start(context.currentTime);
 
 document.addEventListener("mousemove", function(e){
-    //console.log(e.pageX,e.pageY);
-    //console.log(window.innerWidth);
-    //console.log(window.innerHeight);
-    console.log((e.clientX/width) * maxFrequenz);
-    console.log((e.clientY /height) * maxVolume);
     oscillator.frequency.value = (e.clientX/width) * maxFrequenz;
-    gain.gain.value = (e.clientY /height) * maxVolume;
+    gain.gain.value = (e.clientY /height);
 })
 
 
